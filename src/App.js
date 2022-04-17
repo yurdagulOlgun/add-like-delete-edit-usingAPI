657a2309465cee7eab9fc89fec5e66bf03f3d262
Yurdagül OLGUN <yurdagulolgun98@gmail.com>
2022-04-17 03:08:42 +0300

    NavBar and Footer created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import NavBar from './components/base/NavBar';
+import Footer from './components/base/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
+      <Footer/>
     </div>
   );
 }
